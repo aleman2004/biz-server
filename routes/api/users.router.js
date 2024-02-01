a65ab6c5f334276aad17af276290755931d024d5
eman <eman.ahmad.alsayed@gmail.com>
2024-02-01 19:27:56 +0200

    first commit

---
+++ routes/api/users.router.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  loginController,
+  registerController,
+} from "../../controllers/users.controller.js";
+import bodyValidationMiddleware from "../../middlewares/bodyValidation.mw.js";
+import { registerValidation, loginValidation } from "../../validation/validationAdapter.js";
+const router = express.Router();
+
+// http://localhost:3030/api/users
+router.get("/", (req, res) => {
+  res.json("users sub route");
+});
+
+router.post(
+  "/register",
+  bodyValidationMiddleware(registerValidation),
+  registerController
+);
+
+router.post("/login",
+bodyValidationMiddleware(loginValidation),
+loginController);
+
+export default router;
